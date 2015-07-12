11cc500f893d9d579e0a0d02d414e2bb55fbf5a5
Ubuntu <ubuntu@ip-172-31-24-79.eu-west-1.compute.internal>
2015-07-12 23:30:19 +0000

    working

---
+++ blog/application.py
@@ -0,0 +1,23 @@
+import os, sys, json
+import flask
+from flask import request, Response
+
+# New application
+application = flask.Flask(__name__)
+
+# Default route
+@application.route('/')
+def index():
+    return 'Index Page'
+
+@application.route('/about')
+def about():
+    return 'About'
+
+@application.route('/projects')
+def projects():
+    return 'Projects'
+
+if __name__ == '__main__':
+	application.run(host='0.0.0.0')
+
--- setup.sh
+++ setup.sh
@@ -1,15 +1,37 @@
-setenv $APPNAME="myapp"
+#!/bin/bash
+
+# Set up the system
 sudo apt-get install python-dev build-essential python-pip nginx
 sudo apt-get update
-sudo useradd -c 'uwsgi user,,,' -g nginx -d /nonexistent -s /bin/false uwsgi
-sudo pip install virtualenv
-sudo virtualenv --system-site-packages /srv/webapps/$APPNAME/env
-source /srv/webapps/$APPNAME/env/bin/activate
-sudo pip install flask
-deactivate
-sudo mkdir -p /etc/webapps/$APPNAME
-cd /etc/webapps/$APPNAME
-sudo mkdir $APPNAME
-sudo mkdir $APPNAME/static
-sudo touch $APPNAME/__init__.py
+sudo pip install virtualenv uwsgi
 
+# Setup filesystem
+cd ~
+mkdir -p /var/log/uwsgi
+mkdir -p /etc/uwsgi/apps-available
+mkdir -p /etc/uwsgi/apps-enabled
+mkdir -p /var/www/run
+
+# Copy site files
+cd ~/www
+cp -R blog/ /var/www/blog
+sudo chown -R nginx:nginx /var/www
+
+# Copy config files
+cd ~/www
+cp ./config/uwsgi/uwsgi.conf /etc/init/uwsgi.conf
+cp ./config/uwsgi/apps-available/* /etc/uwsgi/apps-available/blog.ini
+cp ./config/nginx/default.conf /etc/nginx/conf.f/default.conf
+
+# Link app
+sudo ln -s /etc/uwsgi/apps-available/blog.ini /etc/uwsgi/apps-enabled/blog.ini
+
+# Setup a virtualenv
+cd ~/www
+virtualenv venv
+. venv/bin/activate
+sudo pip install Flask
+deactivate
+
+# Fire it up
+sudo service nginx start
